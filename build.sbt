d67fca31b8f107c84065a1430d4c2dc039905ea2
Philippe Charrière <ph.charriere@gmail.com>
2018-01-17 07:29:25 +0100

    add circe sample

--- build.sbt
+++ build.sbt
@@ -4,3 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.12.4"
 
+val circeVersion = "0.9.0"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- src/main/scala/garden/bots/Main.scala
+++ src/main/scala/garden/bots/Main.scala
@@ -2,8 +2,24 @@ package garden.bots
 
 import scala.util.{Failure, Success}
 
+// JSON
+import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+
+
 object Main extends App {
 
-  println("👋 Hello 🌍")
+  case class Human(val firstName: String, val lastName: String)
+
+  println("👋 Hello 🌍 ⚠️")
+
+  val bob = Human("Bob", "Morane")
+  val jsonString: String = bob.asJson.toString
+
+  println(jsonString)
+
+  println(
+    decode[Human](jsonString)
+  )
+
 
 }
